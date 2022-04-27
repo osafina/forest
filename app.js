1bdf71503a8978d60cb06d1147f6713af9896f2e
fedmard <fedefmartinez@gmail.com>
2022-04-27 18:52:35 -0300

    Creación de app.js y del servidor, subida de la imagen logo

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const app = express ();
+const path = require ('path');
+app.use (express.static ('public'));
+app.listen (3030, () => console.log ( 'El server funciona en el puerto 3030'))
+app.get ("/", (req, res) => 
+res.sendFile (path.join (__dirname, "/views/home.html")));
+app.get ("/register", (req, res) => res.sendFile (path.join (__dirname, "/views/register.html")))
