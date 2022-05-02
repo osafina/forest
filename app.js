98342f5755f2e913073ca3bc2b1aa4c08964be0a
osafina <josefinaluque92@gmail.com>
2022-05-02 14:27:33 -0300

    registro y home

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express ();
 const path = require ('path');
 app.use (express.static ('public'));
 
+const port = process.env.PORT || 3030;
+
 app.listen (3030, () => console.log ( 'El server funciona en el puerto 3030'))
 
 app.get ("/", (req, res) => 
-res.sendFile (path.join (__dirname, "/views/home.html")))
+res.sendFile (path.join (__dirname, "/views/home.html")));
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+
+});
