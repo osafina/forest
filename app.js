a3f95087ca490331831826e2ff5c3d6e7873b48e
fedmard <fedefmartinez@gmail.com>
2022-05-02 11:20:25 -0300

    actualización de app.js incluyendo la hoja de registro y login

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ app.listen (3030, () => console.log ( 'El server funciona en el puerto 3030'))
 
 app.get ("/", (req, res) => 
 res.sendFile (path.join (__dirname, "/views/home.html")))
+app.get ("/register", (req, res) => 
+res.sendFile (path.join (__dirname, "/views/register.html")))
+app.get ("/login", (req, res) => 
+res.sendFile (path.join (__dirname, "/views/login.html")))
