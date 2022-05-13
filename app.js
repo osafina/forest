61fef229fe69dc5d5eb2ee3fab9872c736ccb707
osafina <josefinaluque92@gmail.com>
2022-05-12 21:05:14 -0300

    reformas

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require ('express');
 const app = express ();
 const path = require ('path');
-const mainRoutes = require ('./routes/mainRoutes')
 app.use (express.static ('public'));
 
 const port = process.env.PORT || 3030;
 
 app.listen (3030, () => console.log ( 'El server funciona en el puerto 3030'))
 
-app.get ("/", mainRoutes);
+app.get ("/", (req, res) => 
+res.sendFile (path.join (__dirname, "/views/home.html")));
 
 app.get ("/register", (req, res) => 
 res.sendFile (path.join (__dirname, "/views/register.html")));
 
 app.get('/producto', (req,res) => {
     res.sendFile(path.join(__dirname,'/views/producto.html'));
+
 });
 
 app.get('/carrito', (req,res) => {
