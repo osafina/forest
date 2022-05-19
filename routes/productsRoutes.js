1d9113e63be3be5711ccd8f222ee19753a26a239
fedmard <fedefmartinez@gmail.com>
2022-05-18 23:13:06 -0300

    controller

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -9,6 +9,7 @@ router.get("/carrito",productsControllers.carrito);
 router.get('/index',productsControllers.index);
 router.get('/crearProducto',productsControllers.create);
 router.post('/index',productsControllers.store);
+router.post('/modificarProducto', )
 
 router.get('index/:id/',productsControllers.detalle)
 module.exports = router;
