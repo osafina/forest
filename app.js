60e17ef9f0521680d37c70e9d1a0621ba23c1af0
osafina <josefinaluque92@gmail.com>
2022-05-23 15:59:02 -0300

    detalles mas fotos distintas

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const bodyParser = require('body-parser');
 
 const app = express ();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use (express.static ('public'));
 
