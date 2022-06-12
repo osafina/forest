ae33051450c5922d224b3d6502c9cba119ec3410
osafina <josefinaluque92@gmail.com>
2022-06-12 20:27:54 -0300

    algo del login

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require ('express');
 const methodOverride =  require('method-override');
-
-const rutasMain = require('./routes/mainRoutes.js');
-const rutasProducto = require('./routes/productsRoutes.js');
-const rutasUsers = require('./routes/usersRoutes.js');
-
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const logMiddleware = require('./middleware/logMiddleware');
-
-
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
 
 
 
@@ -19,17 +12,25 @@ const app = express ();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(methodOverride('_method'));
-
-
 app.use (express.static ('public'));
+app.use(logMiddleware);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(session( {secret:'mensaje secreto'}));
 
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3030;
 
 app.listen (3030, () => console.log ( 'El server funciona en el puerto 3030'))
+
+
+const rutasMain = require('./routes/mainRoutes.js');
+const rutasProducto = require('./routes/productsRoutes.js');
+const rutasUsers = require('./routes/usersRoutes.js');
 
 app.use(logMiddleware);
 app.use ("/", rutasMain);
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,5 +1,6 @@
 const path = require ('path');
 const fs = require('fs');
+const {check, validationResult, body}=require('express-validator');check
 
 
 const usuariosFilePath = path.join(__dirname,'../data/usuariosDataBase.json');
@@ -11,6 +12,27 @@ const usersControllers = {
     ingreso: (req, res,next) => {
     res.render("login")
 
+    },
+    processlogin: (req,res)=> {
+        let errors =validationResult(req);
+
+        if(errors.isEmpty()){
+            for (let i=0;i<usuarios.length;i++){
+                if(usuarios[i].email == req.body.email){
+                    if (bcrypt.compareSync(req.body.password,req.body.password)){
+                        let usuarioAloguearse = usuarios[i];
+                        console.log('éntro bien');
+                        break;
+                    }
+                }
+            }
+            if (usuarioAloguearse == undefined){
+                return res.render('login',{errors: errors.errors})
+            }
+            req.session.usuariologueado =usuarioAloguearse;
+        }else{
+            return res.render('login',{errors});
+        }
     },
 
     registro: (req, res,next) => {
