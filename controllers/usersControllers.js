10ab4f078d6b9b4e82a240065c99cb77c0258957
osafina <josefinaluque92@gmail.com>
2022-08-25 16:09:49 -0300

    base datos

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,116 +1,122 @@
-const path = require ('path');
+const path = require('path');
 const fs = require('fs');
-const bcryptjs = require('bcryptjs');
-const session = require('express-session')
+const db = require('../database/models')
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const User = db.User;
 
 
+const { validationResult } = require('express-validator');
 
-const { validationResult }=require('express-validator');
-
-const User = require('../models/Users');
 
 
 const usersControllers = {
 
-    ingreso: (req, res,next) => {
-    res.render("login");
+    ingreso: (req, res, next) => {
+        res.render("login");
 
     },
-    processregister: (req,res)=> {
+
+    registro: (req, res, next) => {
+        res.render("register");
+
+    },
+
+    index: (req, res) => {
+        res.render('usuarios', { usuarios: usuarios });
+
+    },
+
+    processregister: (req, res) => {
         const resultValidation = validationResult(req);
 
-//si su longitud es mayor a 0, si hay errores, voy a renderizar la vista nuevamente. 
-        if(resultValidation.errors.length > 0) {
-            return res.render('register',{
-//mapped convierte el array en un objeto literal.
+        //si su longitud es mayor a 0, si hay errores, voy a renderizar la vista nuevamente. 
+        if (resultValidation.errors.length > 0) {
+            return res.render('register', {
+                //mapped convierte el array en un objeto literal.
                 errors: resultValidation.mapped(),
-//esta propiedad almacena todo lo qe viene en el require del body.
-                oldData: req.body});
+                //esta propiedad almacena todo lo qe viene en el require del body.
+                oldData: req.body
+            });
         }
-//buscamos usuario que coincida con el email usando el modelo.
-        let userInDB= User.findByField('email', req.body.email);
-
-//revisar validación de si el main ya está registrado      
-         if (userInDB){
-            return res.render('register',{ 
-            errors: {msg:
-            'Este mail ha sido registrado'}, 
-            oldData: req.body
-        },
-         );
-         }
+        //buscamos usuario que coincida con el email usando el modelo.
+        let userInDB = User.findOne({ where: { email: req.body.email } })
+        //revisar validación de si el main ya está registrado      
+        if (userInDB) {
+            return res.render('register', {
+                errors: {
+                    msg:
+                        'Este mail ya ha sido registrado'
+                },
+                oldData: req.body
+            },
+            );
+        }
 
         let userToCreate = {
             ...req.body,
             contrasenia: bcryptjs.hashSync(req.body.contrasenia, 10),
             confirmar: bcryptjs.hashSync(req.body.contrasenia, 10),
-            imagen:req.file
-            
+            imagen: req.file
+
         }
 
-        let userCreated = User.create(userToCreate);
+        let userCreated = User.create(userToCreate)
+            .then(() => res.redirect("/login"))
 
-        return res.redirect('login');
     },
 
-    processLogin : (req,res) => {
-        
-    let userToLogin = User.findByField('email', req.body.email);
+    processLogin: (req, res) => {
 
-		
-    if(userToLogin) {
-        let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.contrasenia);
-        
-        if (isOkThePassword) {
-//Borramos la propiedad password por seguridad
-            delete userToLogin.password
-//Guardamos en la propiedad userLogged al usuario que se logió.
-        req.session.userLogged = userToLogin
+        const resultValidation = validationResult(req);
 
-        if (req.body.remember_user) {
-            res.cookie('userEmail', req.body.email, {maxAge: (1000 * 60) * 5})
+        //si su longitud es mayor a 0, si hay errores, voy a renderizar la vista nuevamente. 
+        if (resultValidation.errors.length > 0) {
+            let userToLogin = User.findOne({ where: { email: req.body.email } })
+                .then(user => {
+                    if (userToLogin) {
+                        let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.contrasenia);
+
+                        if (isOkThePassword) {
+                            //Borramos la propiedad password por seguridad
+                            delete userToLogin.password
+                            //Guardamos en la propiedad userLogged al usuario que se logió.
+                            req.session.userLogged = userToLogin
+
+                            if (req.body.remember_user) {
+                                res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 5 })
+                            }
+
+                            res.redirect('..');
+                        };
+                        //Si no encuentro UserToLogin
+                        return res.render('login', {
+                            errors: {
+                                email: {
+                                    msg: 'Las credenciales son inválidas'
+                                }
+                            }
+
+                        });
+                    }
+
+                    return res.render('login', {
+                        errors: {
+                            email: {
+                                msg: 'No se encuentra este email en nuestra base de datos'
+                            }
+                        }
+
+                    });
+                })
         }
-
-           res.redirect ('..');
-        }; 
-
-//Si no encuentro UserToLogin
-        return res.render('login', {
-            errors: {
-                email: {
-                    msg: 'Las credenciales son inválidas'
-                }
-            }
-            
-        });
-    }
-
-    return res.render('login', {
-        errors: {
-            email: {
-                msg: 'No se encuentra este email en nuestra base de datos'
-            }
-        }
-        
-    });
-    },
-
-
-    registro: (req, res,next) => {
-        res.render("register");
-    
-    },
-
-    index: (req,res) => {
-        res.render('usuarios',{usuarios: usuarios});
-        
     },
     
     logout: (req, res) => {
         res.clearCookie('email');
         req.session.destroy();
         res.redirect('..')
-    } ,
+    },
 
 }
 
